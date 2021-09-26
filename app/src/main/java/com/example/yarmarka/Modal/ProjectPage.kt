9704246e5eea072bd76f98115e22291770e13a37
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-26 22:03:39 +0800

    Описаны pojo классы: Candidate, Project, ProjectPage

---
+++ app/src/main/java/com/example/yarmarka/Modal/ProjectPage.kt
@@ -0,0 +1,16 @@
+package com.example.yarmarka.Modal
+
+data class ProjectPage (
+    var current_page: Int? = null,
+    var first_page_url: String? = null,
+    var from: Int? = null,
+    var to: Int? = null,
+    var total: Int? = null,
+    var prev_page_url: String? = null,
+    var per_page: Int? = null,
+    var path: String? = null,
+    var next_page_url: String? = null,
+    var last_page_url: String? = null,
+    var last_page: Int? = null,
+    var project: Project? = null
+)
