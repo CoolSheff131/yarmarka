0f3e7f6e3193d222815d80dfc547e016ac0b3eed
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-26 22:04:39 +0800

    fix candidate.kt

---
+++ app/src/main/java/com/example/yarmarka/Modal/Candidate.kt
@@ -0,0 +1,12 @@
+package com.example.yarmarka.Modal
+
+data class Candidate (
+    var fio: String? = null,
+    var email: String? = null,
+    var phone: String? = null,
+    var competencies: String? = null,
+    var skill: String? = null,
+    var course: Int? = null,
+    var training_group:  String? = null,
+    var experionce: String? = null
+)
