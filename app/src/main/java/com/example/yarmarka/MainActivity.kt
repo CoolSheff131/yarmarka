71552c5d8286886c52e873f33cf94a1f84d895fa
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-10-31 20:26:36 +0800

    попытка в пуш уведомления

--- app/src/main/java/com/example/yarmarka/MainActivity.kt
+++ app/src/main/java/com/example/yarmarka/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.example.yarmarka
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.example.yarmarka.utils.fm
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        addNotification("test","test" )
+        Toast.makeText(this, " asd",Toast.LENGTH_LONG)
         setTheme(R.style.Theme_Yarmarka)
         setContentView(R.layout.activity_main)
 
         fm = supportFragmentManager
+
+    }
+
+    private fun addNotification(title: String, body: String) {
+        val builder: Notification.Builder? = Notification.Builder(this)
+            .setSmallIcon(R.drawable.launch_screen) //set icon for notification
+            .setContentTitle(title) //set title of notification
+            .setContentText(body) //this is notification message
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        //notification message will get at NotificationView
+        notificationIntent.putExtra("message", body)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, notificationIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        builder?.setContentIntent(pendingIntent)
+        // Add as notification
+        val manager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        manager.notify(0, builder?.build())
+        Log.v("AS","ASFAS")
     }
 }
--- app/src/main/java/com/example/yarmarka/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/yarmarka/ui/login/LoginFragment.kt
@@ -1,10 +1,12 @@
 package com.example.yarmarka.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.yarmarka.R
